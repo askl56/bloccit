56f0a71242e7a5d3405a3383f4171cdde9730fd7
Andrew Scott <amscott@openmailbox.org>
2015-02-06 21:35:01 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Bloccit::Application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
 root to: 'welcome#index'
 end
