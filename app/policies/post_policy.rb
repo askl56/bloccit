a6a8c067f413d3d1dbba11b3e9415426f9d1333e
askl56 <amscott@openmailbox.org>
2015-02-24 04:39:34 +0000

    Complete Authorization Assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,19 @@
  	def index?
  		true
  	end
- end
+  	class Scope < Scope
+    	def resolve
+      		if user.admin?
+        		scope.all
+      		else
+        		scope.where(:published => true)
+      		end
+    	end
+  end
+
+  def update?
+    user.admin? or not post.published?
+  end
+end
+
+end
