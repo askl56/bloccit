681523d955be31fd2a6f37148eb75b9e75dfae9a
askl56 <amscott@openmailbox.org>
2015-02-24 06:56:03 +0000

    Complete Topics and Posts assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Bloccit::Application.routes.draw do
    devise_for :users
   resources :topics do
      resources :posts, except: [:index]
+     resources :summary
    end
   get 'about' => 'welcome#about'
 root to: 'welcome#index'
