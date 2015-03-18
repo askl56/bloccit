abd8d7565930b2245797449a0bd79aa88bec4bb0
askl56 <amscott@openmailbox.org>
2015-03-18 04:21:41 +0000

    Comment Assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @topic = Topic.find(params[:topic_id])
+    # @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     # @comment = @post.comments.create(comment_params)
     @comments = @post.comments
@@ -11,27 +11,27 @@ class CommentsController < ApplicationController
     authorize @post
       if @comment.save
         flash[:notice] = "Comment was saved."
-        redirect_to [@topic, @post]
+        redirect_to [@post.topic, @post]
       else
         flash[:error] = "There was an error saving the comment. Please try again."
         render :new
       end
     end
 
-    def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
-     @comment = @post.comments.find(params[:id])
- 
-     authorize @comment
-     if @comment.destroy
-       flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+  def destroy
+    # @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@post.topic, @post]
      else
-       flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
-     end
-   end
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
 
 private
 
