ee3b411a6a03fea6c2d895ad635ee0e8a12b25eb
askl56 <amscott@openmailbox.org>
2015-03-20 12:27:56 +0000

    Voting stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,29 +1,25 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
-  mount_uploader :image, ImageUploader
 
-  default_scope { order('created_at DESC') }
+  default_scope { order ('rank DESC') }
 
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
+   mount_uploader :image, ImageUploader
 
-def markdown_title
-  markdown_to_html(title)
-end
+  def markdown_title
+    markdown_to_html(title)
+  end
 
-def markdown_body
-  markdown_to_html(body)
-  
-end
+  def markdown_body
+    markdown_to_html(body)
+  end
 
-private
-
-
-  
   def markdown_to_html(markdown)
     renderer = Redcarpet::Render::HTML.new
     extensions = {fenced_code_blocks: true}
@@ -31,4 +27,22 @@ private
     (redcarpet.render markdown).html_safe
   end
 
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
+
+    update_attribute(:rank, new_rank)
+  end
 end
