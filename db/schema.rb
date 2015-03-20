d262a695deb6321651232ceb4ae181528a0881d3
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2015-03-20 14:15:26 +0100

    adds migration for post image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320032645) do
+ActiveRecord::Schema.define(version: 20150320131445) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150320032645) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.float    "rank"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
