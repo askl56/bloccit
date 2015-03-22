e23a074780d1a2730f89588da972fe82dc22fb8c
askl56 <amscott@openmailbox.org>
2015-03-22 02:15:45 +0000

    Complete another interlude assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,29 @@ module ApplicationHelper
     "Andrew Scott"
   end
 
-   def form_group_tag(errors, &block)
+  def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
     else
       content_tag :div, capture(&block), class: 'form-group'
     end
-   end
+  end
+
+  def up_vote_link_classes
+    if current_user.voted(post) && current_user.voted(post).up_vote?
+      voted = 'voted'
+    else
+      voted = ''
+    end
+    "glyphicon glyphicon-chevron-up #{voted}"
+  end
+
+  def down_vote_link_classes
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+      voted = 'voted'
+    else
+      voted = ''
+    end
+    "glyphicon glyphicon-chevron-down #{voted}"
+  end
 end
