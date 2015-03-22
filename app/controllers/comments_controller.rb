8ce8092e18c96ce156f21f109784fd2d0c5db47b
askl56 <amscott@openmailbox.org>
2015-03-22 09:07:45 +0000

    Destroying with AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,31 +9,34 @@ class CommentsController < ApplicationController
     @comment.post = @post
     # authorize @comment
     authorize @post
-      if @comment.save
-        flash[:notice] = "Comment was saved."
-        redirect_to [@post.topic, @post]
-      else
-        flash[:error] = "There was an error saving the comment. Please try again."
-        render :new
-      end
+    if @comment.save
+      flash[:notice] = "Comment was saved."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+      render :new
     end
+  end
 
   def destroy
-    # @topic = Topic.find(params[:topic_id])
+
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
-     else
+    else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
-private
+  private
 
   def comment_params
     params.require(:comment).permit(:body)
